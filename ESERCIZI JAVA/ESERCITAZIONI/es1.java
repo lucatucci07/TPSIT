ec5aa25e90494cbf148c1ece8abdc1264459d47e
lucatucci07 <153677829+lucatucci07@users.noreply.github.com>
2025-01-28 08:58:32 +0000

    UPDATE

---
+++ ESERCIZI JAVA/ESERCITAZIONI/es1.java
@@ -0,0 +1,19 @@
+class Veicolo {
+   
+    private String marca = "Ford";
+    private String modello = "Fiesta";
+    
+    public void StampaDettagli() {
+        System.out.println("La marca è " + marca);
+        System.out.println("Il modello è " + modello);
+    }
+}
+
+public class es1 {
+    public static void main(String[] args) {
+        Veicolo oggetto = new Veicolo();
+        oggetto.StampaDettagli();
+    }
+}
+
+
